18af3f5e2a70447630f389deade7d6783e903cfa
VitalyDron <dronvitaly@gmail.com>
2020-03-24 19:48:58 +0200

    Create class Application

---
+++ src/main/java/com/epam/Application.java
@@ -0,0 +1,10 @@
+package com.epam;
+
+/**
+ * Created by Alex on 23.03.2020.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
